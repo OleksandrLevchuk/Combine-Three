f7b5d99fc800f4e865ca59f48d49772850b83044
anexlader <anexlader@gmail.com>
2024-08-02 23:46:41 +0300

    1

--- docs/index.service.worker.js
+++ docs/index.service.worker.js
@@ -3,7 +3,7 @@
 // that they need an Internet connection to run the project if desired.
 // Incrementing CACHE_VERSION will kick off the install event and force
 // previously cached resources to be updated from the network.
-const CACHE_VERSION = "1722630483|11358688879";
+const CACHE_VERSION = "1722631518|12393592062";
 const CACHE_PREFIX = "Combine Three-sw-cache-";
 const CACHE_NAME = CACHE_PREFIX + CACHE_VERSION;
 const OFFLINE_URL = "index.offline.html";
